fd10341ac980091862ff70734893a9ab62b88c25
yoni1221 <habtie027@gmail.com>
2023-02-16 07:17:42 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - a program that prints printf function
+ *
+ * return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+long int c;
+long long int d;
+char d;
+float f;
+printf("size of char: %lu bytes(S) \n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu bytes(S) \n" (unsigned long)sizeof(b));
+printf("Size of long int: %lu bytes(S) \n" (unsigned long)sizeof(c));
+printf("Size of log long int: %lu bytes(S) \n" (unsigned long)sizeof(d));
+printf("Size of float: %lu bytes(S) \n" (unsigned long)sizeof(f));
+return (0);
+}
