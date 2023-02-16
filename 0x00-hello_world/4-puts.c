760dbd4a070665aff1da4f412e052ddd979d3fa6
yoni1221 <habtie027@gmail.com>
2023-02-16 01:57:15 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programming is like building a multilingual puzzle
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
