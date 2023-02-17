1b01c8bb6d71cf0d3e8d41fd3aa54bf66d1d6266
yoni1221 <habtie027@gmail.com>
2023-02-17 00:00:55 -0800

    aprogram that print ascript using write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
-/*
- * main - Entry
- * return: 1
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is usefu\"  
-	- Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (1);
 }
