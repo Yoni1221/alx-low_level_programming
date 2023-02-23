f4157bc72c220c694382384402efac67ca32e7d7
yoni1221 <habtie027@gmail.com>
2023-02-23 09:11:13 -0800

    a function that prints a triangle, followed by a new line.

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
 * print_number - print number using putchar
 * @n: parameter hold  numbers
 * Return: void
 */
-
 void print_number(int n)
 {
 if (n >= 10)
@@ -15,8 +13,6 @@ _putchar(n / 10 + '0');
 else
 {
 _putchar(n % 10  + '0');
-
 }
-
 }
 
