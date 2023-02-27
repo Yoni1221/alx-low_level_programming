391cf9dca2579daba57407caaa815c1e8f6154ab
yoni1221 <habtie027@gmail.com>
2023-02-27 04:01:40 -0800

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - return the length of a string
+ * @a : name of array
+ * @n : size of array
+ * Return: length of string
+ *
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i == n - 1)
+{
+continue;
+}
+else
+{
+printf(", ");
+}
+}
+printf("\n");
+
+}
