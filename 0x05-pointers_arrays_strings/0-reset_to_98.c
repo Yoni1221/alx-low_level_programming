60a59eb039ff7840ae8bbf222ece2665a45f821d
yoni1221 <habtie027@gmail.com>
2023-02-26 22:57:55 -0800

    aprogram that update pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * reset_to_98- set the integer to 98
+ * @n: a pointer the integer we want to set to 98
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void _puts(char *str);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void print_array(int *a, int n);
+void rev_string(char *s);
+int _strlen(char *s);
+void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *str);
+/* MAIN_H */
