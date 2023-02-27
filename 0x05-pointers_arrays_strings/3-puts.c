d05d48d63874a073450a65c3f871bc6d9fa990e7
yoni1221 <habtie027@gmail.com>
2023-02-26 23:19:05 -0800

    a function that prints a string, followed by a new line, to stdout.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+
+/**
+ * _puts - return the length of a string
+ * @str : Parameter accept  string
+ * Return: nothng
+ *
+ */
+
+void _puts(char *str)
+{
+int c;
+for (c = 0; str[c] != '\0'; c++)
+{
+_putchar(str[c]);
+
+}
+_putchar('\n');
+}
