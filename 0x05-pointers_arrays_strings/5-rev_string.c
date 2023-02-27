c1581db03016217aa535230d13994b2b7f33b84e
yoni1221 <habtie027@gmail.com>
2023-02-27 04:55:28 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,17 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * rev_string - return the length of a string
- * @s : Parameter accept  string
- * Return: nothng
+ * main - check the code
  *
+ * Return: Always 0.
  */
-void rev_string(char *s)
+int main(void)
 {
-int len = 0;
-char p;
-while (s[len])
-{
-len++;
-}
-len--;
-while (len >= 0)
-{
-p = s[len];
-len--;
-}
+    char s[10] = "My School";
+
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 int _putchar(char c);
 void _puts(char *str);
 void reset_to_98(int *n);
@@ -8,5 +11,5 @@ int _strlen(char *s);
 void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-char *_strcpy(char *dest, char *str);
-/* MAIN_H */
+char *_strcpy(char *dest, char *src);
+#endif /* MAIN_H */
