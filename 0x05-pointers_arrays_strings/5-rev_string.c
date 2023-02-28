bbf92841dccbccc024c8e8500e3fef3bf09fcc65
yoni1221 <habtie027@gmail.com>
2023-02-28 04:04:07 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,27 +3,7 @@
 
 /**
  * rev_string - return the length of a string
- * @s : Parameter accept  string
- * Return: nothng
- *
+ * @s : Parameter acce
+ * }
  */
 
-void rev_string(char *s)
-{
-int len = 0;
-char p;
-while (s[len])
-{
-len++;
-}
-len--;
-
-
-while (len >= 0)
-{
-p = s[len];
-
-len--;
-}
-
-}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,4 +12,5 @@ void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 #endif /* MAIN_H */
