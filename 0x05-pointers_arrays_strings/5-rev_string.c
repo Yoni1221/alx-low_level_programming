fe33a56c4b0dfec4de3dd7051cf17130480f6ff8
yoni1221 <habtie027@gmail.com>
2023-02-28 06:40:47 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,28 @@
-
 #include "main.h"
 
 /**
  * rev_string - return the length of a string
- * @s : Parameter acce
- * }
+ * @s : Parameter accept  string
+ * Return: nothng
+ *
  */
 
+void rev_string(char *s)
+{
+int len = 0;
+char p;
+while (s[len])
+{
+len++;
+}
+len--;
+
+
+while (len >= 0)
+{
+p = s[len];
+
+len--;
+}
+
+}
