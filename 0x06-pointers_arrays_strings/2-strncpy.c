3843ec389a63845d428a1c2866076a5c3041c10a
yoni1221 <habtie027@gmail.com>
2023-03-01 23:20:05 -0800

    copy the string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,13 +7,19 @@
  * @n : byte
  * Return: a pointer to the resulting string dest
  */
-
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n);
 {
-int i;
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[i] = src[i];
-for (; i < n; i++)
-dest[i] = '\0';
+int j;
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++
+}
 return (dest);
 }
