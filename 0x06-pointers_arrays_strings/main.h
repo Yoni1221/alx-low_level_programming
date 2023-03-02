8e842be565014a76531348d678d5a5961c2ab65a
yoni1221 <habtie027@gmail.com>
2023-03-02 00:03:03 -0800

    hello srtring

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,3 @@
-main.h
-
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -16,6 +11,4 @@ char *rot13(char *a);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
-#endif
 
