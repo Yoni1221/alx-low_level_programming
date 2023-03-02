ea9137d0c22f7d21d9ab8be5b0e29bb7ac2944f9
yoni1221 <habtie027@gmail.com>
2023-03-01 23:29:08 -0800

    string copy function

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,7 +7,7 @@
  * @n : byte
  * Return: a pointer to the resulting string dest
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n);
 {
 int i;
 for (i = 0; i < n && src[i] != '\0'; i++)
