6effe2f2665e9b2b2df261a5cb3171ce20b545ec
yoni1221 <habtie027@gmail.com>
2023-03-30 04:57:08 -0700

    You are allowed to use the printf function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
-
+void printfirst(void) __attribute__ ((constructor));
 /**
- * first - prints a sentence before the main
- * function is executed
+ * printfirst - print before main
+ *
+ * Return: void
  */
-void first(void)
+void printfirst(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
